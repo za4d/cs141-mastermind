38685b377ff2a79dccab5113006abcac621110c7
za4d <za4d@github.com>
2019-01-27 23:20:31 +0000

    - Clean up code - move temp code to temp.hs

--- src/Game.hs
+++ src/Game.hs
@@ -4,137 +4,130 @@
 --------------------------------------------------------------------------------
 
 -- | This module should contain your game code.
-module Game where
-import Data.List
-import Data.Ix
---------------------------------------------------------------------------------
+  module Game where
+  import Data.List
+  import Data.Ix
+  --------------------------------------------------------------------------------
 
--- | The number of pegs in a code.
-pegs :: Int
-pegs = 4
+  -- | The number of pegs in a code.
+  pegs :: Int
+  pegs = 4
 
--- | Symbols are represented by characters.
-type Symbol = Char
+  -- | Symbols are represented by characters.
+  type Symbol = Char
 
--- | The available symbols in the game.
-symbols :: [Symbol]
-symbols = ['a'..'f']
+  -- | The available symbols in the game.
+  symbols :: [Symbol]
+  symbols = ['a'..'f']
 
--- | A code is represented by a list of symbols.
-type Code = [Symbol]
+  -- | A code is represented by a list of symbols.
+  type Code = [Symbol]
 
--- | Guesses are scored using coloured and white markers. The first component
--- of the pair gives the number of coloured markers and the right component
--- gives the number of white markers.
-type Score = (Int, Int)
+  -- | Guesses are scored using coloured and white markers. The first component
+  -- of the pair gives the number of coloured markers and the right component
+  -- gives the number of white markers.
+  type Score = (Int, Int)
 
--- | A player is either human or computer-controlled.
-data Player = Human | Computer
+  -- | A player is either human or computer-controlled.
+  data Player = Human | Computer
 
--- | The first codemaker in a play session.
-codemaker :: Player
-codemaker = Human
+  -- | The first codemaker in a play session.
+  codemaker :: Player
+  codemaker = Human
 
--- | The first guess the AI will make.
-firstGuess :: Code
-firstGuess = "aabb"
+  -- | The first guess the AI will make.
+  firstGuess :: Code
+  firstGuess = "aabb"
 
---------------------------------------------------------------------------------
+  --------------------------------------------------------------------------------
 
--- | Determines whether a score indicates that a guess was correct or not.
-------------------------------------
-{-- A simple test that checks whether the score says all the peg have the right
-    colour and position i.e. max (pegs) number of coloured markers and no white markers --}
-correctGuess :: Score -> Bool
-correctGuess s = (s == (pegs,0))
+  -- | Determines whether a score indicates that a guess was correct or not.
+  ------------------------------------
+  {-- A simple test that checks whether the score says all the peg have the right
+      colour and position i.e. max (pegs) number of coloured markers and no white markers --}
+  correctGuess:: Score -> Bool
+  correctGuess s = (s == (pegs,0))
 
--- | This function should check that the code entered by a human player is
--- valid. In other words, it should have the length given by `pegs` and it
--- should only contain valid symbols.
-------------------------------------
-{-- If the code is the correct length and all elements are also elements of symbols
-    else its invalid --}
-validateCode :: Code -> Bool
-validateCode xs = if length xs == pegs
-                  then all (\x -> x `elem` symbols) xs --
-                  else False
+-- TODO: Remove elemOf ?
+  -- -- | Same as elem function but takes list first then the term
+  -- -- Used so the higher order functions is neater
+  -- elemOf :: (Foldable t, Eq a) => t a -> a -> Bool
+  -- elemOf = flip elem
 
-
--- | All possible codes.
-------------------------------------
--- [Your explanation]
-codes :: [Code]
-codes = permute pegs symbols
-
-permute :: Int -> [a] -> [[a]]
-permute 0 _ = [[]]
-permute n xs = [ x:ys | x <- xs, ys <- (permute (n-1) xs)]
-
--- | All possible scores.
-------------------------------------
--- [Your explanation][NO CONDITIONAL NEEDED]
-results :: [Score]
-results = nub $ map test codes
-  where
-    test = score $ take pegs (cycle symbols)
--- | Scores a guess against a code. Symbols which are in the right place
--- and of the right type score a coloured marker. Symbols which are of the
--- right type but in the wrong place score a white marker.
-------------------------------------
--- [Your explanation]
-score :: Code -> Code -> Score
-score code guess = (c,w-c)
-  where
-    c = countColored code guess
-    w = countWhite' code guess
-
-countColored :: Code -> Code -> Int
-countColored [] [] = 0
-countColored (x:xs) (y:ys)
-  | x == y    = 1 + countColored xs ys
-  | otherwise = countColored xs ys
-
-countWhite :: Code -> Code -> Int
-countWhite code guess = length $ filter (\x -> x `elem` guess) code
+  -- | This function should check that the code entered by a human player is
+  -- valid. In other words, it should have the length given by `pegs` and it
+  -- should only contain valid symbols.
+  ------------------------------------
+  {--
+  If the code is the correct length and
+  if all elements are also elements of symbols
+  else its invalid
+  --}
+  validateCode :: Code -> Bool
+  validateCode xs
+    | length xs == pegs   = all (`elem` symbols) xs
+    | otherwise           = False
 
 
-countWhite' :: Code -> Code -> Int
-countWhite' [] _ = 0
-countWhite' (x:xs) guess
-  | x `elem` guess  = 1 + countWhite' xs (delete x guess)
-  | otherwise       = countWhite' xs guess
+  -- | All possible codes.
+  ------------------------------------
+  -- [Your explanation]
+  codes :: [Code]
+  codes = permute pegs symbols
+
+  permute :: Int -> [a] -> [[a]]
+  permute 0 _ = [[]]
+  permute n xs = [ x:ys | x <- xs, ys <- (permute (n-1) xs)]
 
 
+  -- | All possible scores.
+  ------------------------------------
+  -- [Your explanation][NO CONDITIONAL NEEDED]
+  results :: [Score]
+  results = nub $ map test codes
+    where
+      test = score $ take pegs (cycle symbols)
 
 
-score' :: Code -> Code -> Score
-score' code guess = (c,w-c)
-  where
-    c = filterL (uncurry (==)) (zip guess code)
-    w = filterL (`elem` guess) code
+  -- | Scores a guess against a code. Symbols which are in the right place
+  -- and of the right type score a coloured marker. Symbols which are of the
+  -- right type but in the wrong place score a white marker.
+  ------------------------------------
+  -- [Your explanation]
+  score            :: Code -> Code -> Score
+  score code guess = (c,w-c)
+    where
+      c = countColored code guess
+      w = countWhite code guess
 
-filterL :: (a -> Bool) -> [a] -> Int
-filterL p xs = length $ filter p xs
+  countColored :: Code -> Code -> Int
+  countColored [] [] = 0
+  countColored (x:xs) (y:ys)
+    | x == y    = 1 + countColored xs ys
+    | otherwise = countColored xs ys
+
+  countWhite :: Code -> Code -> Int
+  countWhite [] _ = 0
+  countWhite (x:xs) guess
+    | x `elem` guess  = 1 + countWhite xs (delete x guess)
+    | otherwise       = countWhite xs guess
 
 
--- compareLists:: Eq a => (a -> a -> Bool) -> [a] -> [a] -> Int
--- countWhite p listA listB = filter (\x -> x `elem` listB) listA
+  -- | Chooses the next guess. If there is only one option left, choose it.
+  -- Otherwise, calculate the hit score for each code and choose the code
+  -- with the largest hit score.
+  ------------------------------------
+  -- [Your explanation]
+  nextGuess :: [Code] -> Code
+  nextGuess s = undefined
 
--- | Chooses the next guess. If there is only one option left, choose it.
--- Otherwise, calculate the hit score for each code and choose the code
--- with the largest hit score.
-------------------------------------
--- [Your explanation]
-nextGuess :: [Code] -> Code
-nextGuess s = undefined
+  -- | Remove all codes from the remaining possibilities which would result in
+  -- a different score for the guess if they were the code.
+  -- In other words, given the set of remaining possible codes, narrow it down
+  -- to those which would produce the same score we got from the codemaker.
+  ------------------------------------
+  -- [Your explanation]
+  eliminate :: Score -> Code -> [Code] -> [Code]
+  eliminate lastScore guess codes = undefined
 
--- | Remove all codes from the remaining possibilities which would result in
--- a different score for the guess if they were the code.
--- In other words, given the set of remaining possible codes, narrow it down
--- to those which would produce the same score we got from the codemaker.
-------------------------------------
--- [Your explanation]
-eliminate :: Score -> Code -> [Code] -> [Code]
-eliminate lastScore guess codes = undefined
-
---------------------------------------------------------------------------------
+  --------------------------------------------------------------------------------
--- src/Temp.hs
+++ src/Temp.hs
@@ -2,3 +2,24 @@
 module Temp where
 
   import Data.List
+
+
+
+
+
+  score' :: Code -> Code -> Score
+  score' code guess = (c,w-c)
+    where
+      c = filterL (uncurry (==)) (zip guess code)
+      w = filterL (`elem` guess) code
+
+  filterL :: (a -> Bool) -> [a] -> Int
+  filterL p xs = length $ filter p xs
+
+
+
+  countWhite :: Code -> Code -> Int
+  countWhite code guess = length $ filter (\x -> x `elem` guess) code
+
+  compareLists:: Eq a => (a -> a -> Bool) -> [a] -> [a] -> Int
+  countWhite p listA listB = filter (\x -> x `elem` listB) listA
